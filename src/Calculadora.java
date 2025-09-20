2be3f18c44f45d7760f6fb6b64bf2c9bc8dc7b79
irenecg22 <ireneca032006@gmail.com>
2025-09-20 17:43:38 +0200

    calculadora terminada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,41 @@
 
+
+import java.util.Scanner;
+
 public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Introduce el primer número: ");
+        int num1 = sc.nextInt();
 
-	}
+        System.out.print("Introduce el operador (+ - * /): ");
+        char signo = sc.next().charAt(0);
 
+        System.out.print("Introduce el segundo número: ");
+        int num2 = sc.nextInt();
+        
+
+        double resultado = 0;
+
+        switch (signo) {
+            case '+': 
+            	resultado = num1 + num2; break;
+            case '-': resultado = num1 - num2; break;
+            case '*': resultado = num1 * num2; break;
+            case '/': 
+                if (num2 != 0) {
+                	resultado = num1 / num2;
+                	
+                }	
+                else System.out.println("No se puede dividir entre 0");
+                break;
+            default: 
+                System.out.println("Operador no válido");
+                return;
+        }
+
+        System.out.println("Resultado: " + resultado);
+        sc.close();
+    }
 }
